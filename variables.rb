757f0d6dd200ef7583dc979589c8f77f0b9fb271
Sid Mann <sjmann2@outlook.com>
2022-06-07 17:51:18 -0500

    Add puts statements

--- variables.rb
+++ variables.rb
@@ -1,10 +1,16 @@
 username = "sjmann2"
 first_name = "sid"
+puts first_name
 pet_name = "kitty"
 age = 26
+puts age
+puts age + 10
 number_pets = 2
 number_kids = 0
 rent = 1295.50
+puts rent + 300
 allowance = 10.75
-under_70 = true
+puts allowance
 has_paid = false
+under_70 = true
+under_70 = false
